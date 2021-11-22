daad72a89a900cbbbef65023ab238a60c0e1670d
Akshay Hallagade <akshayhallagade2612@gmail.com>
2021-11-22 17:52:22 +0530

    imroved UI

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -7,41 +7,59 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Material(
       color: Colors.white,
-      child: Column(
-        children: [
-          Image.asset("assests/images/login.png"),
-          SizedBox(
-            height: 20.0,
-          ),
-          Text(
-            "Welcome",
-            style: TextStyle(
-              fontSize: 22,
-              fontWeight: FontWeight.bold,
+      child: Center(
+        child: Column(
+          children: [
+            Image.asset("assests/images/login.png"),
+            SizedBox(
+              height: 20.0,
             ),
-          ),
-          SizedBox(
-            height: 20.0,
-          ),
-          TextFormField(
-            decoration: InputDecoration(
-                hintText: "Enter Username", labelText: "Username"),
-          ),
-          TextFormField(
-            obscureText: true,
-            decoration: InputDecoration(
-                hintText: "Enter Password", labelText: "Password"),
-          ),
-          ElevatedButton(
-            child: Text("Login"),
-            style: ButtonStyle(
-                // foregroundColor: TextButton.styleFrom(),
-                ),
-            onPressed: () {
-              print("Hey Akshay");
-            },
-          )
-        ],
+            Text(
+              "Welcome",
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                hintText: "Enter Username",
+                labelText: "Username",
+                contentPadding: EdgeInsets.fromLTRB(20, 10, 10, 20),
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(32.0)),
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            TextFormField(
+              obscureText: true,
+              decoration: InputDecoration(
+                hintText: "Enter Password",
+                labelText: "Password",
+                contentPadding: EdgeInsets.fromLTRB(20, 10, 10, 20),
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(32.0)),
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            ElevatedButton(
+              child: Text("Login"),
+              style: ButtonStyle(
+                  // foregroundColor: TextButton.styleFrom(),
+                  ),
+              onPressed: () {
+                print("Hey Akshay");
+              },
+            )
+          ],
+        ),
       ),
     );
   }
